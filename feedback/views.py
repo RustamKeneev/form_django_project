71a9301d0650412bb70ca19a5b74482cd928a9d0
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-16 23:46:34 +0600

    Manual form validation

--- feedback/views.py
+++ feedback/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponseRedirect
 
 
 def index(request):
-    return render(request, 'feedback/feedback.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        print(name)
+        if len(name) == 0 or len(name) > 20:
+            return render(request,'feedback/feedback.html', context={'got_error': True})
+        surname = request.POST['surname']
+        return HttpResponseRedirect('/done')
+    return render(request, 'feedback/feedback.html', context={'got_error': False})
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
