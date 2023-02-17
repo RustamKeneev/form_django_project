13980be1a5f6454567ed68e5722ed2d8ce89c1f1
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-17 18:47:10 +0600

    Manual setting of form fields and styles

--- feedback/forms.py
+++ feedback/forms.py
@@ -7,6 +7,6 @@ class FeedbackForm(forms.Form):
         'min_length': 'Слишком мало символов',
         'required': 'Укажите хотябы 2 символа',
     })
-    # surname = forms.CharField()
-    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
+    surname = forms.CharField()
+    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
     rating = forms.IntegerField(label="Рейтинг", max_value=5, min_value=1)
