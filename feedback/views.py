3f278e2b9f21bbf11232d694c83cd4152e46f626
RustamKeneev <dr.rustamkeneev@gmail.com>
2023-02-17 19:51:01 +0600

    Saving the form in the django database

--- feedback/views.py
+++ feedback/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
-
+from .models import Feedback
 
 def index(request):
-    # form = FeedbackForm()
     if request.method == 'POST':
-        name = request.POST['name']
-        print(name)
         form = FeedbackForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            feedback = Feedback(
+                name=form.cleaned_data['name'],
+                surname=form.cleaned_data['surname'],
+                feedback=form.cleaned_data['feedback'],
+                rating=form.cleaned_data['rating']
+            )
+            feedback.save()
         return HttpResponseRedirect('/done')
     else:
         form = FeedbackForm()
